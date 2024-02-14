d84019ce503df23e2a7ed9bc31df9b6323de6724
HHamlet <hh.harutyunyan@mail.ru>
2024-02-14 23:44:02 +0400

    added class : ShowBooks, ShowAuthor for pydentic validation

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI
 import uvicorn
+from pydantic import BaseModel
 from sqlalchemy import select
 from models import BookModel, BookAuthorModel, AuthorModel
 from db import async_session
 
 app = FastAPI(title="Library model on FastAPI")
+
+
+class ShowBooks(BaseModel):
+    id: int
+    title: str
+    year_first_published: int
+
+
+class ShowAuthor(BaseModel):
+    id: int
+    first_name: str
+    last_name: str
 
 
 async def get_request(statement):
@@ -15,14 +28,14 @@ async def get_request(statement):
         return result
 
 
-@app.get("/books")
+@app.get("/books", response_model=list[ShowBooks])
 async def get_book_list():
     statement = select(BookModel)
     result = await get_request(statement)
     return result
 
 
-@app.get("/books/{title}")
+@app.get("/books/{title}", response_model=list[ShowBooks])
 async def get_book(title):
     statement = select(BookModel).where(BookModel.title.like("%" + title + "%"))
     result = await get_request(statement)
@@ -36,7 +49,7 @@ async def get_author_books():
     return result
 
 
-@app.get("/author")
+@app.get("/author", response_model=list[ShowAuthor])
 async def get_author(name, lastname):
     statement = select(AuthorModel).where(
         AuthorModel.first_name == name, AuthorModel.last_name == lastname
