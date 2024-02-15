76bdb3808a1a56710834ac6d368a9edb09182799
HHamlet <hh.harutyunyan@mail.ru>
2024-02-15 22:34:08 +0400

    short refactoring

--- app.py
+++ app.py
@@ -1,23 +1,17 @@
 from fastapi import FastAPI
 import uvicorn
-from pydantic import BaseModel
 from sqlalchemy import select
-from models import BookModel, BookAuthorModel, AuthorModel, Book_CopiesModel
+from models import (
+    BookModel,
+    BookAuthorModel,
+    AuthorModel,
+    Book_CopiesModel,
+    ShowBooks,
+    ShowAuthor,
+)
 from db import async_session
 
 app = FastAPI(title="Library model on FastAPI")
-
-
-class ShowBooks(BaseModel):
-    id: int
-    title: str
-    year_first_published: int
-
-
-class ShowAuthor(BaseModel):
-    id: int
-    first_name: str
-    last_name: str
 
 
 async def get_request(statement):
@@ -26,6 +20,15 @@ async def get_request(statement):
             temp = await session.scalars(statement)
             result = temp.all()
         return result
+
+
+async def get_book(title):
+    statement = select(BookModel).where(BookModel.title == title)
+    async with async_session() as session:
+        async with session.begin():
+            temp = await session.scalars(statement)
+            result = temp.first()
+    return result
 
 
 @app.get("/books", response_model=list[ShowBooks])
@@ -39,16 +42,6 @@ async def get_book_list():
 async def get_books(title):
     statement = select(BookModel).where(BookModel.title.like("%" + title + "%"))
     result = await get_request(statement)
-    return result
-
-
-@app.get("/books/title", response_model=list[ShowBooks])
-async def get_book(title):
-    statement = select(BookModel).where(BookModel.title == title)
-    async with async_session() as session:
-        async with session.begin():
-            temp = await session.scalars(statement)
-            result = temp.first()
     return result
 
 
@@ -135,4 +128,4 @@ async def get_author(name, lastname):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
