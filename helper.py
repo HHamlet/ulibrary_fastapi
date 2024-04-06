e6ec84eb9ab06ce720652b95dded01f1c52ace98
HHamlet <hh.harutyunyan@mail.ru>
2024-04-06 15:39:32 +0400

    create cache for paginate & add to redis

--- helper.py
+++ helper.py
@@ -1,12 +1,16 @@
+import json
 import math
 from typing import Annotated
+import redis
 from fastapi.param_functions import Query
 from fastapi.params import Depends
 from pydantic import BaseModel
-from sqlalchemy import select
+from sqlalchemy import select, inspect
 from sqlalchemy.exc import NoResultFound
 
 from db import async_session
+
+redis_client = redis.Redis(host="localhost", port=6379, decode_responses=True)
 
 
 async def get_request_all(statement):
@@ -40,11 +44,27 @@ async def pagination_param(
     return Paginate(page=page, per_page=per_page)
 
 
+async def object_as_dict(obj):
+    return {c.key: getattr(obj, c.key) for c in inspect(obj).mapper.column_attrs}
+
+
 async def pagination(paginate: Annotated[Paginate, Depends(pagination_param)], dbmodel):
-    offset = (paginate.page - 1) * paginate.per_page
-    statement = select(dbmodel).limit(int(paginate.per_page)).offset(offset)
-    result = await get_request_all(statement)
+    cache_name = f"{dbmodel.__name__}_{paginate.page}_{paginate.per_page}"
+    pagination_result = redis_client.get(cache_name)
+    if pagination_result is None:
+
+        offset = (paginate.page - 1) * paginate.per_page
+        statement = select(dbmodel).limit(int(paginate.per_page)).offset(offset)
+        result = await get_request_all(statement)
+        dict_books = [await object_as_dict(book) for book in result]
+        print(dict_books)
+        redis_client.set(cache_name, json.dumps(dict_books))
+        return dict_books
+    else:
+        books = json.loads(pagination_result)
+        print(books)
+        return books
     # total_page = math.ceil(
     #     len(await get_request_all(select(dbmodel))) / int(paginate.per_page)
     # )
-    return result
+    # return result
